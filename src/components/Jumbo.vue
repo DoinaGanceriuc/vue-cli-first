7f9c92f987398d5ee64abccd0f3bd57c42435476
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-10 15:53:11 +0100

    add jumbo component

---
+++ src/components/Jumbo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="container">
+      <img src="https://picsum.photos/500/600" alt="random image">
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.container {
+  width: 1140px;
+  margin: 2rem auto;
+}
+
+</style>
