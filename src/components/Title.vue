e7027d1a0dcd37bba0be48bc58eef0bd357c4e1c
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-10 15:31:50 +0100

    add title component

---
+++ src/components/Title.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>La mia prima app con Vue CLI!</h1>
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped lang="scss">
+h1 {
+  margin: 40px 0 0;
+}
+</style>
